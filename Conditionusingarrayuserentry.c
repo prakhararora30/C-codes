73f480a86be9d987d0520a054aa6615ac01f8611
Prakhar <prakhar.akgec@outlook.com>
2024-12-11 15:57:10 +0530

    Update Conditional.using.array.user.defined.entries.c

--- Conditionusingarrayuserentry.c
+++ Conditionusingarrayuserentry.c
@@ -1,11 +1,14 @@
 #include<stdio.h>
 int main(){
+    
     int i, j, total=0, marks[5];
-    printf("enter marks of five subjects:");
+    
+    printf("enter marks of five subjects:");        //user entries for marks
     for(i=0; i<5; i++){
         scanf("%d", &marks[i]);
     }
-    for(i=0; i<5; i++){
+    
+    for(i=0; i<5; i++){        //result evaluation
         if(marks[i]>60){
             printf("%d ", marks[i]);
             printf("pass");
@@ -17,9 +20,12 @@ int main(){
             printf("\n");
         }
     }
-    for(j=0; j<5; j++){
+    
+    for(j=0; j<5; j++){        //sum of marks
         total = total + marks[j];
     }
+    
     printf("%d", total);
+    
     return 0;
 }
