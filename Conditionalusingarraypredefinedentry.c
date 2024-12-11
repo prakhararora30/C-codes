7af3599054efd38a8da1198d01797c098fafc021
Prakhar <prakhar.akgec@outlook.com>
2024-12-11 15:54:41 +0530

    Update Conditional.using.array.predefined.entries.c

--- Conditionalusingarraypredefinedentry.c
+++ Conditionalusingarraypredefinedentry.c
@@ -1,7 +1,9 @@
 #include<stdio.h>
 int main(){
+    
     int i, j, total, marks[5]={90, 58, 84, 93, 58};
-    for(i=0; i<5; i++){
+    
+    for(i=0; i<5; i++){        //deciding the result
         if(marks[i]>60){
             printf("%d ", marks[i]);
             printf("pass");
@@ -13,8 +15,10 @@ int main(){
             printf("\n");
         }
     }
+    
     total=0;
-    for(j=0; j<5; j++){
+    
+    for(j=0; j<5; j++){        //sum of marks
         total = total + marks[j];
     }
     printf("%d", total);
