fac9e4dbd0f7222fb7602228aaf8dd1321cc7e93
Prakhar <prakhar.akgec@outlook.com>
2025-02-17 21:16:26 +0530

    Create Selection Sort Ascending Order.c

---
+++ Sorting/Selection Sort Ascending Order.c
@@ -0,0 +1,39 @@
+#include<stdio.h>
+int main(){
+    int n, i, j, t, min;
+
+    printf("enter the size of array: ");
+    scanf("%d", &n);
+
+    int a[n];
+
+    printf("enter the values of array: ");
+
+    for(i=0; i<n; i++){
+        scanf("%d", &a[i]);
+    }
+
+    for(i=0; i<n; i++){
+        
+        min = i;
+
+        for(j=i+1; j<n; j++){
+            if(a[min] > a[j]){
+
+                min=j;
+                t=a[i];
+                a[i]=a[min];
+                a[min]=t;
+            }
+        }
+
+    }
+
+    printf("the sorted ascending array: ");
+
+    for(i=0; i<n; i++){
+        printf("%d ", a[i]);
+    }
+
+    return 0;
+}
